fa3aef8a06851c0311ee2c743380d3fc94cecdac
Hakan Dındış <hakandindis@gmail.com>
2023-04-25 22:07:35 +0300

    splash screen implemented

--- app/src/main/java/org/hakandindis/cryptoapp/MainActivity.kt
+++ app/src/main/java/org/hakandindis/cryptoapp/MainActivity.kt
@@ -2,12 +2,14 @@ package org.hakandindis.cryptoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
